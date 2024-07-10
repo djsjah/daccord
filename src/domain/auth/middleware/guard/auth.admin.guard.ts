b01b0219eae9891a8477b2e4898f622b264a6fae
djsjah <awesom-e4000@mail.ru>
2024-07-10 13:56:08 +0300

    fixed authAdminGuard

--- src/domain/auth/middleware/guard/auth.admin.guard.ts
+++ src/domain/auth/middleware/guard/auth.admin.guard.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import authGuard from './auth.guard';
 
 const authAdminGuard = async (req: Request, res: Response, next: NextFunction) => {
-  authGuard(req, res, () => {
+  await authGuard(req, res, () => {
     if (req.user?.role !== 'admin') {
       return res.status(401).send("Unauthorized - you are not signed in as admin");
     }
